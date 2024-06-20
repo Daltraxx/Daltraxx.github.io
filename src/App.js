bb105a32f76e6cbd4e9e508bc9ddd6a5148c7cba
Dalton Pettus <89285609+Daltraxx@users.noreply.github.com>
2024-06-20 08:51:41 -0700

    Set up to begin styling App component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import styles from './App.module.css';
 import LandingPage from './components/LandingPage/LandingPageIntro';
 
 function App() {
   return (
-    <div className="App">
+    <div className={styles.main}>
       <header className="App-header">
         <LandingPage />
       </header>
