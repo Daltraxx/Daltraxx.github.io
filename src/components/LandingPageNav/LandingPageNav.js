a7cee37e79788776402abbf9827a2e643321566f
Dalton Pettus <89285609+Daltraxx@users.noreply.github.com>
2024-06-20 18:08:38 -0700

    Further style nav links

--- src/components/LandingPageNav/LandingPageNav.js
+++ src/components/LandingPageNav/LandingPageNav.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import styles from './LandingPageNav.module.css';
 import './LandingPageNav.module.css';
 
 function LandingPageNav() {
     return (
         <nav>
             <ul>
-                <li key="about"><a href="#">About</a></li>
-                <li key="resume"><a href="#">Resume</a></li>
-                <li key="projects"><a href="#">Projects</a></li>
-                <li key="contact"><a href="#">Contact</a></li>
+                <li key="about"><a href="#" className={styles.leftNavButton}>About</a></li>
+                <li key="resume"><a href="#" className={styles.leftNavButton}>Resume</a></li>
+                <li key="projects"><a href="#" className={styles.rightNavButton}>Projects</a></li>
+                <li key="contact"><a href="#" className={styles.rightNavButton}>Contact</a></li>
             </ul>
         </nav>
     )
