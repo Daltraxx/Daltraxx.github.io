d68c967f5a7e9df379bbc2da02bfd4a610a101df
Dalton Pettus <89285609+Daltraxx@users.noreply.github.com>
2024-06-21 19:32:49 -0700

    Add href to social links and some styling

--- src/components/Header/SocialLinks/SocialLinks.js
+++ src/components/Header/SocialLinks/SocialLinks.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import styles from './SocialLinks.module.css';
 
 function SocialLinks() {
     return (
-        <ul>
-            <li><a>GitHub</a></li>
-            <li><a>LinkedIn</a></li>
+        <ul className={styles.socialLinksContainer}>
+            <li><a href="https://github.com/Daltraxx" target="_blank">GitHub</a></li>
+            <li><a href="https://www.linkedin.com/in/dalton-pettus/" target="_blank">LinkedIn</a></li>
         </ul>
     )
 }
