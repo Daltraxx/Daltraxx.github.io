d7122375e580107199a85cf6a1f5d81eba418442
Dalton Pettus <89285609+Daltraxx@users.noreply.github.com>
2024-06-21 20:12:29 -0700

    Refactor App.js to use switch statement (getBody function) to render correct page

--- src/App.js
+++ src/App.js
@@ -17,6 +17,19 @@ function App() {
     console.log(target.name);
   }
 
+  const getBody = (page) => {
+    switch (page) {
+      case 'about':
+        return <h1>About</h1>;
+      case 'resume':
+        return <h1>Resume</h1>;
+      case 'projects':
+        return <h1>Projects</h1>;
+      case 'contact':
+        return <h1>Contact</h1>;
+    }
+  }
+
 
   if (page === 'landing') {
     return (
@@ -29,34 +42,14 @@ function App() {
         </main>
       </div>
     );
-  } 
-
-
-  //merge at least these
-  if (page === 'about') {
+  } else {
     return (
-      <Header handlePageChange={handlePageChange}/>
-    );
+      <>
+        <Header handlePageChange={handlePageChange}/>
+        {getBody(page)}
+      </>
+    )
   }
-
-  if (page === 'resume') {
-    return (
-      <h1>Resume</h1>
-    );
-  }
-
-  if (page === 'projects') {
-    return (
-      <h1>Projects</h1>
-    );
-  }
-
-  if (page === 'contact') {
-    return (
-      <h1>Contact</h1>
-    );
-  }
-  
 }
 
 export default App;
