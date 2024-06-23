22df957737fe50c2c8cbc8ab3628068a28718802
Dalton Pettus <89285609+Daltraxx@users.noreply.github.com>
2024-06-23 15:46:29 -0700

    Style nav buttons

--- src/components/Header/Nav/Nav.js
+++ src/components/Header/Nav/Nav.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import styles from './Nav.module.css';
 
+//add logic for highlighting current page
+
 function Nav({handlePageChange}) {
     return (
         <nav className={styles.nav}>
             <ul>
-                <li key="about"><a href="#" className={styles.leftNavButton} onClick={handlePageChange} name="about">About</a></li>
-                <li key="resume"><a href="#" className={styles.leftNavButton} onClick={handlePageChange} name="resume">Resume</a></li>
-                <li key="projects"><a href="#" className={styles.rightNavButton} onClick={handlePageChange} name="projects">Projects</a></li>
-                <li key="contact"><a href="#" className={styles.rightNavButton} onClick={handlePageChange} name="contact">Contact</a></li>
+                <li key="about"><a href="#" className={styles.navButton} onClick={handlePageChange} name="about">About</a></li>
+                <li key="resume"><a href="#" className={styles.navButton} onClick={handlePageChange} name="resume">Resume</a></li>
+                <li key="projects"><a href="#" className={styles.navButton} onClick={handlePageChange} name="projects">Projects</a></li>
+                <li key="contact"><a href="#" className={styles.navButton} onClick={handlePageChange} name="contact">Contact</a></li>
             </ul>
         </nav>
     )
