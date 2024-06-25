f4ba98765dcb4ac8ca6d1699c233e942a623d5e9
Dalton Pettus <89285609+Daltraxx@users.noreply.github.com>
2024-06-24 18:14:31 -0700

    Further style quick facts and extra information on About component

--- src/components/Body/About/About.js
+++ src/components/Body/About/About.js
@@ -30,19 +30,25 @@ function About() {
     return (
         <section className={styles.aboutContainer}>
             <h2>About</h2>
-            <div>
-                <h3>Quick Facts</h3>
-                <ul className={styles.quickFacts}>
-                    <li id='01' onMouseOver={handleHover}>Began learning first program language, Python, in May of 2020.</li>
-                    <li id='02' onMouseOver={handleHover}>Started studying programming in earnest* while working in QA from 2021 to 2024.</li>
-                    <li id='03' onMouseOver={handleHover}>Knew hardly a thing about computers until his first QA job until 2018.</li>
-                    <li id='04' onMouseOver={handleHover}>His four loves in this world are fitness, books, video games, and programming.</li>
-                    <li id='05' onMouseOver={handleHover}>Excelled in academics growing up, but didn't discover any viable or enticing career paths until learning to code.</li>
-                </ul>
-            </div>
-            <div className={styles.extraInfo}>
-                <h3>More Information</h3>
-                {getExtraInfo()}
+            <div className={styles.factsContainer}>
+                <div className={styles.half}>
+                    <div>
+                        <h3>Quick Facts</h3>
+                        <ul className={styles.quickFacts}>
+                            <li id='01' onMouseOver={handleHover}>Began learning first program language, Python, in May of 2020.</li>
+                            <li id='02' onMouseOver={handleHover}>Started studying programming in earnest* while working in QA from 2021 to 2024.</li>
+                            <li id='03' onMouseOver={handleHover}>Knew hardly a thing about computers until his first QA job until 2018.</li>
+                            <li id='04' onMouseOver={handleHover}>His four loves in this world are fitness, books, video games, and programming.</li>
+                            <li id='05' onMouseOver={handleHover}>Excelled in academics growing up, but didn't discover any viable or enticing career paths until learning to code.</li>
+                        </ul>
+                    </div>
+                </div>
+                <div className={styles.half}>
+                    <div className={styles.factsExtraInfo}>
+                        <h3>More Information</h3>
+                        {getExtraInfo()}
+                    </div>
+                </div>
             </div>
             <div className={styles.lifeStory}>
                 <h3>Life Story</h3>
