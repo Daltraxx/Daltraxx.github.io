5a2f78cafa7643b582ba94559c4f0fee1dc5f475
Dalton Pettus <89285609+Daltraxx@users.noreply.github.com>
2024-06-26 15:52:22 -0700

    Add placeholder text and initial styling for longer introduction in About

--- src/components/Body/About/About.js
+++ src/components/Body/About/About.js
@@ -8,9 +8,15 @@ function About() {
         <section className={styles.aboutContainer}>
             <h2>About</h2>
             <QuickFacts />
-            <div className={styles.lifeStory}>
+            <section className={styles.lifeStoryContainer}>
                 <h3>Life Story</h3>
-            </div>
+                <div className={styles.lifeStoryText}>
+                    <p>Hello, there The angel from my nightmare The shadow in the background of the morgue The unsuspecting victim Of darkness in the valley We can live like Jack and Sally if we want</p>
+                    <p>Where you can always find me And we'll have Halloween on Christmas And in the night, we'll wish this never ends We'll wish this never ends</p>
+                    <p>Where are you and I'm so sorry I cannot sleep, I cannot dream tonight I need somebody and always This sick, strange darkness Comes creeping on so haunting everytime</p>
+                    <p>And as I stared I counted the webs from all the spiders Catching things and eating their insides Like indecision to call you and hear your voice of treason Will you come home and stop this pain tonight? Stop this pain tonight</p>
+                </div>
+            </section>
         </section>
     );
 }
