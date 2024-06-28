1c6ba7dce6f6552a074fc3253fde0ec491a6d54e
Dalton Pettus <89285609+Daltraxx@users.noreply.github.com>
2024-06-27 18:17:26 -0700

    Create workExperience array and use to map to jsx for Resume

--- src/components/Body/Resume/Resume.js
+++ src/components/Body/Resume/Resume.js
@@ -1,10 +1,37 @@
 import React from 'react';
 import styles from './Resume.module.css';
+import workExperience from './resources/work-experience';
+
+
 
 function Resume() {
+    const workExperienceJSX = workExperience.map(job => {
+        return (
+            <section>
+                <h4>{job.title}</h4>
+                <p>{job.company}</p>
+                <p>{job.dates}</p>
+                <p>{job.description}</p>
+            </section>
+        );
+    });
+    console.log(workExperienceJSX);
+
     return (
         <section>
             <h2 className='page-header'>Resume</h2>
+            <section>
+                <h3>Statement</h3>
+                <p>
+                    I believe in an eclectic approach to both life and work that always strives for mastery. 
+                    On and off the clock I labor to consume information that will aid my propensity for growth and contribution, 
+                    as well as my understanding of how my impact may best be felt by any organization I service.
+                </p>
+            </section>
+            <section>
+                <h3>Work Experience</h3>
+                {workExperienceJSX}
+            </section>
         </section>
     );
 }
---
+++ src/components/Body/Resume/resources/work-experience.js
@@ -0,0 +1,24 @@
+//create array of objects to contain work experience for use in Resume
+
+const workExperience = [
+    {
+        title: 'QA Lead',
+        company: 'Robot Cache',
+        dates: 'Jul 2021 - Jan 2024',
+        description: 'Plaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaceholder'
+    },
+    {
+        title: 'QA Lead',
+        company: 'Robot Cache',
+        dates: 'Jul 2021 - Jan 2024',
+        description: 'Plaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaceholder'
+    },
+    {
+        title: 'QA Lead',
+        company: 'Robot Cache',
+        dates: 'Jul 2021 - Jan 2024',
+        description: 'Plaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaceholder'
+    }
+];
+
+export default workExperience;
