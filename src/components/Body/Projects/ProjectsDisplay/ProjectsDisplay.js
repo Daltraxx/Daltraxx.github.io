5fbf9ccafe0a855c4e908e46fb919716aa1b36e0
Dalton Pettus <89285609+Daltraxx@users.noreply.github.com>
2024-06-29 15:53:53 -0700

    Continue modifying styles of ProjectsDisplay

--- src/components/Body/Projects/ProjectsDisplay/ProjectsDisplay.js
+++ src/components/Body/Projects/ProjectsDisplay/ProjectsDisplay.js
@@ -4,15 +4,22 @@ import styles from './ProjectsDisplay.module.css';
 function ProjectsDisplay({ projectsData }) {
     const projectsJSX = projectsData.map(project => {
         return (
-            <section>
-                <img src={project.image}/>
-                <h3>{project.name}</h3>
-                <p>{project.description}</p>
-                <a href={project.github} target="_blank">Github Link</a>
-                <h4>Technologies Used:</h4>
-                <ul>
-                    {project.technologies.map(tech => <li>{tech}</li>)}
-                </ul>
+            <section className={styles.projectContainer}>
+                <div className={styles.imageContainer} style={{backgroundImage: 'url' + `(${project.image})`}}>
+                    <div className={styles.shader}>
+                        <div className={styles.titleInfo}>
+                            <h3>{project.name}</h3>
+                            <h4>Technologies Used:</h4>
+                            <ul>
+                                {project.technologies.map(tech => <li>{tech}</li>)}
+                            </ul>
+                            <a href={project.github} target="_blank">Github Link</a>
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.projectInfo}>
+                    <p>{project.description}</p>
+                </div>
             </section>
         );
     });
