27976223c1766b793e1ea8a8485c21960ea775b6
Dalton Pettus <89285609+Daltraxx@users.noreply.github.com>
2024-07-01 11:35:04 -0700

    Further style Projects page

--- src/components/Body/Projects/ProjectsDisplay/ProjectsDisplay.js
+++ src/components/Body/Projects/ProjectsDisplay/ProjectsDisplay.js
@@ -2,24 +2,30 @@ import React from 'react';
 import styles from './ProjectsDisplay.module.css';
 
 function ProjectsDisplay({ projectsData }) {
+
+    //get jsx for each project by mapping from projectsData array of objects
     const projectsJSX = projectsData.map(project => {
         return (
             <section className={styles.projectContainer} key={`project` + projectsData.indexOf(project)}>
-                <div className={styles.imageContainer} style={{backgroundImage: 'url' + `(${project.image})`}}>
-                    <div className={styles.shader}>
-                        <div className={styles.titleInfo}>
-                            <h3>{project.name}</h3>
-                            <h4>Technologies Used:</h4>
-                            <ul>
-                                {project.technologies.map(tech => <li key={`${tech}`}>{tech}</li>)}
-                            </ul>
-                            <a href={project.github} target="_blank">Github Link</a>
+                <section className={styles.projectInfoHalf}>
+                    <div className={styles.imageContainer} style={{backgroundImage: 'url' + `(${project.image})`}}>
+                        <div className={styles.shader}>
+                            <div className={styles.titleInfo}>
+                                <h3>{project.name}</h3>
+                                <h4>Technologies Used:</h4>
+                                <ul>
+                                    {project.technologies.map(tech => <li key={`${tech}`}>{tech}</li>)}
+                                </ul>
+                                <a href={project.github} target="_blank">View on GitHub</a>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div className={styles.projectInfo}>
-                    <p>{project.description}</p>
-                </div>
+                </section>
+                <section className={styles.projectInfoHalf}>
+                    <div className={styles.projectDetails}>
+                        <p>{project.description}</p>
+                    </div>
+                </section>
             </section>
         );
     });
