38dad7ae38ec5c9de7fe337f08d629670fdf2e75
Dalton Pettus <89285609+Daltraxx@users.noreply.github.com>
2024-07-01 14:36:36 -0700

    Add logic to highlight currently selected quick fact

--- src/components/Body/About/QuickFacts/QuickFacts.js
+++ src/components/Body/About/QuickFacts/QuickFacts.js
@@ -2,12 +2,23 @@ import React, { useState } from 'react';
 import styles from './QuickFacts.module.css';
 
 function QuickFacts({ verbosity }) {
-    const [fact, setfact] = useState(null);
+    const [fact, setFact] = useState(null);
 
     const handleClick = ({ target }) => {
-        setfact(prev => target.id);
+        //remove highlighting of previous fact if there is one
+        if (fact) {
+            console.log(document.getElementById(fact));
+            document.getElementById(fact).style = '';
+        }
+
+        //continue to highlight currently selected fact
+        target.style.backgroundColor = 'hsla(0, 0%, 0%, .3)';
+        
+        //set new fact
+        setFact(prev => target.id);
     }
 
+    //render More Infromation conditionally based on currently selected fact
     const getExtraInfo = () => {
         switch (fact) {
             case '01':
@@ -29,14 +40,14 @@ function QuickFacts({ verbosity }) {
     return (
         <section className={styles.factsContainer}>
             <div className={verbosity >= 25 ? styles.half : styles.hide}>
-                <div>
+                <div className='bullet-points'>
                     <h3>The Bullet Points</h3>
                     <ul>
-                        <li id='01' onClick={handleClick}>Began learning first program language, Python, in May of 2020.</li>
-                        <li id='02' onClick={handleClick}>Started studying programming in earnest* while working in QA from 2021 to 2024.</li>
-                        <li id='03' onClick={handleClick}>Knew hardly a thing about computers until his first QA job until 2018.</li>
-                        <li id='04' onClick={handleClick}>His four loves in this world are fitness, books, video games, and programming.</li>
-                        <li id='05' onClick={handleClick}>Excelled in academics growing up, but didn't discover any viable or enticing career paths until learning to code.</li>
+                        <li><span id='01' onClick={handleClick}>Began learning first program language, Python, in May of 2020.</span></li>
+                        <li><span id='02' onClick={handleClick}>Started studying programming in earnest* while working in QA from 2021 to 2024.</span></li>
+                        <li><span id='03' onClick={handleClick}>Knew hardly a thing about computers until his first QA job in 2018.</span></li>
+                        <li><span id='04' onClick={handleClick}>His four loves in this world are fitness, books, video games, and programming.</span></li>
+                        <li><span id='05' onClick={handleClick}>Excelled in academics growing up, but didn't discover any viable or enticing career paths until learning to code.</span></li>
                     </ul>
                 </div>
             </div>
