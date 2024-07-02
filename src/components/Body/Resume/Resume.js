94fef162289e54e161f9fef73844c3963c6cae38
Dalton Pettus <89285609+Daltraxx@users.noreply.github.com>
2024-07-01 17:44:09 -0700

    Style and format Resume page

--- src/components/Body/Resume/Resume.js
+++ src/components/Body/Resume/Resume.js
@@ -8,22 +8,11 @@ import certifications from './resources/certifications';
 
 
 function Resume() {
-    //map workExperience object array to JSX
-    const workExperienceJSX = workExperience.map(job => {
-        return (
-            <section>
-                <h4>{job.title}</h4>
-                <p>{job.company}</p>
-                <p>{job.dates}</p>
-                <p>{job.description}</p>
-            </section>
-        );
-    });
 
     //map skills object array to JSX
     const skillsJSX = skills.map(skill => {
         return (
-            <section>
+            <section className={styles.skill}>
                 <h4>{skill.name}</h4>
                 <p>{skill.description}</p>
             </section>
@@ -37,7 +26,17 @@ function Resume() {
         )
     });
     
-    
+    //map workExperience object array to JSX
+    const workExperienceJSX = workExperience.map(job => {
+        return (
+            <section className={styles.job}>
+                <h4>{job.title}</h4>
+                <p>{job.company}</p>
+                <p>{job.dates}</p>
+                <p>{job.description}</p>
+            </section>
+        );
+    });
 
     return (
         <section>
@@ -54,32 +53,40 @@ function Resume() {
                 <h3>Skills</h3>
                 {skillsJSX}
             </section>
-            <section className={`${styles.resumeSection}`}>
+            <section className={`${styles.resumeSection} ${styles.flexRow}`}>
                 <h3>Education</h3>
-                <section>
-                    <h4>Codecademy</h4>
-                    <p>2020 - <em>Ongoing</em></p>
-                    <p>
-                        Description hereeeeeeeeeeeeeeeeeeee.
-                    </p>
-                    <h5>Certifications</h5>
-                    <ul>
-                        {certificationsJSX}
-                    </ul>
+                <section className={styles.educationThird}>
+                    <div className={styles.universitySanDiego}>
+                        <h4>University of San Diego</h4>
+                        <p>Major in Political Science, Minor in Psychology</p>
+                        <p>2012 - 2016</p>
+                        <p>
+                            My university education was genuinely formative in my growing understanding of this world and its complexities, 
+                            and what my role may be in it. My present and future may not be rooted in the above subjects, 
+                            however I only feel immense gratitude for all that has set me on my current trajectory, 
+                            including the skills that I have accumulated along the way.
+                        </p>
+                    </div>
                 </section>
-                <section>
-                    <h4>University of San Diego</h4>
-                    <p>Major in Political Science, Minor in Psychology</p>
-                    <p>2012 - 2016</p>
-                    <p>
-                        My university education was genuinely formative in my growing understanding of this world and its complexities, 
-                        and what my role may be in it. My present and future may not be rooted in the above subjects, 
-                        however I only feel immense gratitude for all that has set me on my current trajectory, 
-                        including the skills that I have accumulated along the way.
-                    </p>
+                <section className={`${styles.educationThird}`}>
+                    <div className={`${styles.codecademyDescription}`}>
+                        <h4>Codecademy</h4>
+                        <p>2020 - <em>Ongoing</em></p>
+                        <p>
+                            Description hereeeeeeeeeeeeeeeeeeee.
+                        </p>
+                    </div>
+                </section>
+                <section className={`${styles.educationThird} ${styles.flexRow}`}>
+                    <div className={`${styles.codecademyCertifications}`}>
+                        <h4>Certifications</h4>
+                        <ul>
+                            {certificationsJSX}
+                        </ul>
+                    </div>
                 </section>
             </section>
-            <section className={`${styles.resumeSection}`}>
+            <section className={`${styles.resumeSection} ${styles.flexRow}`}>
                 <h3>Work Experience</h3>
                 {workExperienceJSX}
             </section>
