167e7ce88532a8e608c7c9ff0dcbfbb9d1fb6fd2
Dalton Pettus <89285609+Daltraxx@users.noreply.github.com>
2024-07-05 19:39:10 -0700

    Move experience and skill icon arrays to resources folder and export to SkillCarousel

--- src/components/LandingPage/SkillCarousel/SkillCarousel.js
+++ src/components/LandingPage/SkillCarousel/SkillCarousel.js
@@ -1,20 +1,30 @@
 import React from 'react';
 import styles from './SkillCarousel.module.css';
 
+import { skillIcons, experienceIcons } from './resources/icons';
+
 
 function SkillCarousel() {
     //create array for displaying "proficient in" icons
-    const skillIcons = [{description: "JavaScript", src: "/assets/images/js-icon.png"}, {description: "React", src: "/assets/images/react-icon.png"}, {description: "CSS", src: "/assets/images/css-icon.png"}, {description: "HTML", src: "/assets/images/html-icon.png"}];
     const skillIconsDisplay = [];
     for(let skill of skillIcons) {
-        skillIconsDisplay.push(<div className={styles.iconContainer} key={skill.description}><img src={skill.src} alt={skill.description} title={skill.description} /></div>);
+        skillIconsDisplay.push
+        (
+            <div className={styles.iconContainer} key={skill.description}>
+                <img src={skill.src} alt={skill.description} title={skill.description} />
+            </div>
+        );
     };
 
     //create array for displaying "experience in" icons
-    const experienceIcons = [{description: "Node.js", src: "/assets/images/node-js-icon.png"}, {description: "Typescript", src: "/assets/images/typescript-icon.png"}, {description: "handlebars.js", src: "/assets/images/handlebars-icon.png"}, {description: "Python", src: "/assets/images/python-icon.png"}, {description: "Bootstrap", src: "/assets/images/bootstrap-icon.png"}, {description: "Angular.js", src: "/assets/images/angular-icon.png"}, {description: "C++", src: "/assets/images/c-plus-plus-icon.png"}];
     const experienceIconsDisplay = [];
-    for(let experience of experienceIcons) {
-        experienceIconsDisplay.push(<div className={styles.iconContainer} key={experience.description}><img src={experience.src} alt={experience.description} title={experience.description} /></div>);
+    for (let experience of experienceIcons) {
+        experienceIconsDisplay.push
+        (
+            <div className={styles.iconContainer} key={experience.description}>
+                <img src={experience.src} alt={experience.description} title={experience.description} />
+            </div>
+        );
     };
     
     return (
---
+++ src/components/LandingPage/SkillCarousel/resources/icons.js
@@ -0,0 +1,18 @@
+const skillIcons = [
+    {description: "JavaScript", src: "/assets/images/js-icon.png"}, 
+    {description: "React", src: "/assets/images/react-icon.png"}, 
+    {description: "CSS", src: "/assets/images/css-icon.png"}, 
+    {description: "HTML", src: "/assets/images/html-icon.png"}
+];
+
+const experienceIcons = [
+    {description: "Node.js", src: "/assets/images/node-js-icon.png"}, 
+    {description: "Typescript", src: "/assets/images/typescript-icon.png"}, 
+    {description: "handlebars.js", src: "/assets/images/handlebars-icon.png"}, 
+    {description: "Python", src: "/assets/images/python-icon.png"}, 
+    {description: "Bootstrap", src: "/assets/images/bootstrap-icon.png"}, 
+    {description: "Angular.js", src: "/assets/images/angular-icon.png"}, 
+    {description: "C++", src: "/assets/images/c-plus-plus-icon.png"}
+];
+
+export { skillIcons, experienceIcons };
