7699c6f18638b8bf158617c4257096125e6e6a37
Dalton Pettus <89285609+Daltraxx@users.noreply.github.com>
2024-07-07 22:14:34 -0700

    "Finish" styling Bio

--- src/components/Body/About/Bio/Bio.js
+++ src/components/Body/About/Bio/Bio.js
@@ -3,14 +3,34 @@ import styles from './Bio.module.css';
 
 function Bio({ verbosity }) {
     return (
-        <section className={styles.lifeStoryContainer}>
-            <h3 className={verbosity < 60 ? styles.hide : ''}>Longer Bio</h3>
-            <div className={styles.lifeStoryText}>
-                <p className={verbosity < 70 ? styles.hide : ''} >Hello, there The angel from my nightmare The shadow in the background of the morgue The unsuspecting victim Of darkness in the valley We can live like Jack and Sally if we want</p>
-                <p className={verbosity < 80 ? styles.hide : ''}>Where you can always find me And we'll have Halloween on Christmas And in the night, we'll wish this never ends We'll wish this never ends</p>
-                <p className={verbosity < 90 ? styles.hide : ''}>Where are you and I'm so sorry I cannot sleep, I cannot dream tonight I need somebody and always This sick, strange darkness Comes creeping on so haunting everytime</p>
-                <p className={verbosity < 100 ? styles.hide : ''}>And as I stared I counted the webs from all the spiders Catching things and eating their insides Like indecision to call you and hear your voice of treason Will you come home and stop this pain tonight? Stop this pain tonight</p>
-            </div>
+        <section className={styles.bioContainer}>
+            <h3 className={verbosity < 55 ? styles.hide : ''}>Longer Bio</h3>
+            <section className={styles.bioText}>
+                <section className={verbosity < 60 ? styles.none : styles.bioCol}>
+                    <div className={verbosity < 65 ? styles.none : styles.bioParagraph}>
+                        <p>Hello, there The angel from my nightmare The shadow in the background of the morgue The unsuspecting victim Of darkness in the valley We can live like Jack and Sally if we want</p>
+                    </div>
+                    <div className={verbosity < 70 ? styles.none : styles.bioParagraph}>
+                        <p>Where you can always find me And we'll have Halloween on Christmas And in the night, we'll wish this never ends We'll wish this never ends</p>
+                    </div>
+                </section>
+                <section className={verbosity < 75 ? styles.none : styles.bioCol}>
+                    <div className={verbosity < 80 ? styles.none : styles.bioParagraph}>
+                        <p>Hello, there The angel from my nightmare The shadow in the background of the morgue The unsuspecting victim Of darkness in the valley We can live like Jack and Sally if we want</p>
+                    </div>
+                    <div className={verbosity < 85 ? styles.none : styles.bioParagraph}>
+                        <p>Hello, there The angel from my nightmare The shadow in the background of the morgue The unsuspecting victim Of darkness in the valley We can live like Jack and Sally if we want</p>
+                    </div>
+                </section>
+                <section className={verbosity < 90 ? styles.none : styles.bioCol}>
+                    <div className={verbosity < 95 ? styles.none : styles.bioParagraph}>
+                        <p>Hello, there The angel from my nightmare The shadow in the background of the morgue The unsuspecting victim Of darkness in the valley We can live like Jack and Sally if we want</p>
+                    </div>
+                    <div className={verbosity < 100 ? styles.none : styles.bioParagraph}>
+                        <p>And as I stared I counted the webs from all the spiders Catching things and eating their insides Like indecision to call you and hear your voice of treason Will you come home and stop this pain tonight? Stop this pain tonight</p>
+                    </div>
+                </section>
+            </section>
         </section>
     )
 }
