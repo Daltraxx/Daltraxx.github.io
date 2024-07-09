eaa66af67df67069bb0b0957b048a4c234ea5a68
Dalton Pettus <89285609+Daltraxx@users.noreply.github.com>
2024-07-08 21:07:12 -0700

    "Finish" styling Resume page

--- src/components/Body/Resume/Resume.js
+++ src/components/Body/Resume/Resume.js
@@ -34,7 +34,7 @@ function Resume() {
             <section className={styles.job} key={`job${workExperience.indexOf(job)}`}>
                 <h4>{job.title}</h4>
                 <p>{job.company}</p>
-                <p>{job.dates}</p>
+                <p className={styles.dateRange}>{job.dates}</p>
                 <p>{job.description}</p>
             </section>
         );
@@ -58,10 +58,10 @@ function Resume() {
             <section className={`${styles.resumeSection} ${styles.flexRow}`}>
                 <h3>Education</h3>
                 <section className={styles.educationThird}>
-                    <div className={styles.universitySanDiego}>
+                    <div className={`${styles.universitySanDiego} ${styles.flexColumn}`}>
                         <h4>University of San Diego</h4>
                         <p>Major in Political Science, Minor in Psychology</p>
-                        <p>2012 - 2016</p>
+                        <p className={styles.dateRange}>2012 - 2016</p>
                         <p>
                             My university education was genuinely formative in my growing understanding of this world and its complexities, 
                             and what my role may be in it. My present and future may not be rooted in the above subjects, 
@@ -71,16 +71,16 @@ function Resume() {
                     </div>
                 </section>
                 <section className={`${styles.educationThird}`}>
-                    <div className={`${styles.codecademyDescription}`}>
+                    <div className={`${styles.codecademyDescription} ${styles.flexColumn}`}>
                         <h4>Codecademy</h4>
-                        <p>2020 - <em>Ongoing</em></p>
+                        <p className={styles.dateRange}>2020 - <em>Continuous</em></p>
                         <p>
                             Description hereeeeeeeeeeeeeeeeeeee.
                         </p>
                     </div>
                 </section>
-                <section className={`${styles.educationThird} ${styles.flexRow}`}>
-                    <div className={`${styles.codecademyCertifications}`}>
+                <section className={`${styles.educationThird}`}>
+                    <div className={`${styles.codecademyCertifications} ${styles.flexColumn}`}>
                         <h4>Certifications</h4>
                         <ul>
                             {certificationsJSX}
