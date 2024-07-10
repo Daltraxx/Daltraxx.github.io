931afe0945524be88ee1d887ee25294ec088d923
Dalton Pettus <89285609+Daltraxx@users.noreply.github.com>
2024-07-10 14:47:23 -0700

    Up to 3 emojis render when verbosity is low enough

--- src/components/Body/About/About.js
+++ src/components/Body/About/About.js
@@ -13,13 +13,20 @@ function About() {
     }
 
     const emojiImg = <img src="/assets/images/zipper-mouth-emoji.png" className={styles.emoji} name="zipped mouth emoji" alt="zipped mouth emoji"/>;
+    const emojiJSX = (
+        <section className={styles.emojiContainer}>
+            {emojiImg}
+            {verbosity <= 5 && emojiImg}
+            {verbosity < 1 && emojiImg}
+        </section>
+    );
 
     //render less or more content depending on verbosity
     return (
         <section className={styles.aboutContainer}>
             <h2 className='page-header'>About</h2>
             <VerbositySlider handleChange={handleChange} verbosity={verbosity}/>
-            {verbosity <= 10 && emojiImg}
+            {verbosity <= 10 && emojiJSX}
             {verbosity >= 20 && <QuickFacts verbosity={verbosity} />}
             {verbosity >= 50 && <Bio verbosity={verbosity} />}
         </section>
