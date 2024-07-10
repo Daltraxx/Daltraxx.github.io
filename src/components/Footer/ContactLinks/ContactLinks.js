d2f55cf5f737f0e535e7f477b22a6ae08e1c6d98
Dalton Pettus <89285609+Daltraxx@users.noreply.github.com>
2024-07-10 13:09:24 -0700

    "Finish" styling footer

--- src/components/Footer/ContactLinks/ContactLinks.js
+++ src/components/Footer/ContactLinks/ContactLinks.js
@@ -4,11 +4,33 @@ import styles from './ContactLinks.module.css';
 import handleEmail from '../../../utils/handleEmail';
 
 function ContactLinks() {
+
+    const linkedInLink = <a 
+        href="https://www.linkedin.com/in/dalton-pettus/" 
+        target="_blank" 
+        className={styles.contactButton} 
+        aria-label="LinkedIn link">
+        LinkedIn
+        </a>;
+
+    const emailLink = <a 
+        href='#!' 
+        className={styles.contactButton} 
+        onClick={handleEmail} 
+        id='contact-link-footer' 
+        aria-label="email button">
+        Email
+        </a>;
+        
     return (
         <section className={styles.contactLinksSection}>
             <ul>
-                <li><a href="https://www.linkedin.com/in/dalton-pettus/" target="_blank" className={styles.contactButton} aria-label="LinkedIn link">LinkedIn</a></li>
-                <li><a href='#!' className={styles.contactButton} onClick={handleEmail} id='contact-link-footer' aria-label="email button">Email</a></li>
+                <li>
+                    {linkedInLink}
+                </li>
+                <li>
+                    {emailLink}
+                </li>
             </ul>
         </section>
     )
