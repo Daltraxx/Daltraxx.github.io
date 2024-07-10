2e58e358fadd35afe0b329d0cc99616513523dcd
Dalton Pettus <89285609+Daltraxx@users.noreply.github.com>
2024-07-10 14:32:41 -0700

    Refactor About component and adjust emoji height/top-margin
    Much less lines of code

--- src/components/Body/About/About.js
+++ src/components/Body/About/About.js
@@ -13,41 +13,17 @@ function About() {
     }
 
     const emojiImg = <img src="/assets/images/zipper-mouth-emoji.png" className={styles.emoji} name="zipped mouth emoji" alt="zipped mouth emoji"/>;
-    if (verbosity <= 10) {
-        return (
-            <section className={styles.aboutContainer}>
-                <h2 className='page-header'>About</h2>
-                <VerbositySlider handleChange={handleChange} verbosity={verbosity}/>
-                {emojiImg}
-            </section>
-        );
-    } else if (verbosity <= 20) {
-        return (
-            <section className={styles.aboutContainer}>
-                <h2 className='page-header'>About</h2>
-                <VerbositySlider handleChange={handleChange} verbosity={verbosity}/>
-            </section>
-        );
-    } else if (verbosity < 60) {
-        return (
-            <section className={styles.aboutContainer}>
-                <h2 className='page-header'>About</h2>
-                <VerbositySlider handleChange={handleChange} verbosity={verbosity}/>
-                <QuickFacts verbosity={verbosity} />
-            </section>
-        );
-    } else if (verbosity >= 60) {
-        return (
-            <section className={styles.aboutContainer}>
-                <h2 className='page-header'>About</h2>
-                <VerbositySlider handleChange={handleChange} verbosity={verbosity}/>
-                <QuickFacts verbosity={verbosity} />
-                <Bio verbosity={verbosity} />
-            </section>
-        );
-    }
 
-    
+    //render less or more content depending on verbosity
+    return (
+        <section className={styles.aboutContainer}>
+            <h2 className='page-header'>About</h2>
+            <VerbositySlider handleChange={handleChange} verbosity={verbosity}/>
+            {verbosity <= 10 && emojiImg}
+            {verbosity >= 20 && <QuickFacts verbosity={verbosity} />}
+            <Bio verbosity={verbosity} />
+        </section>
+    );
 }
 
 export default About;
