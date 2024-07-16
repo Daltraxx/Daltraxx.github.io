3553924b928b9325ae46ab9eb2dd7b999f1d6fd6
Dalton Pettus <89285609+Daltraxx@users.noreply.github.com>
2024-07-15 19:54:44 -0700

    Change project image for portfolio site

--- src/components/Body/Projects/resources/projects-data.js
+++ src/components/Body/Projects/resources/projects-data.js
@@ -11,6 +11,14 @@ const projectsData = [
             During the course of the project I learned about and paid close attention to the pleasant and functional displaying of information, 
             adapting to predicted user actions like automatically requerying when the sort option is changed,
             and ensuring the site is responsive to multiple screen dimensions.`,
+        /*
+        bullets: [
+            "Queries Yelp API based on user input with search parameters and sort options",
+            "Anticipates certain user interactions, like requerying previous search if sort is changed",
+            "Renders and presents businesses and their info in an organized and appealing way",
+            "Responsive and accessible"
+            ],
+        */
         github: 'https://github.com/Daltraxx/ravenous',
         image: '/assets/images/ravenous-project.png',
         technologies: ['JavaScript', 'React', 'Yelp API', 'CSS', 'HTML']
@@ -20,7 +28,7 @@ const projectsData = [
         description: 
             `It is worth placing this very site here because of the variety of skills involved in making it. Built with React and making heavy use of props and hooks,
             the app features multiple pages and dynamic interactions. It is built to be responsive and accessible. I especially enjoyed exploring skeuomorphic touches for many of the buttons,
-            and managing state to dynamically render some of the pages. It is also written to be easy to update.
+            and managing state to dynamically render some of the pages with fun if unnecessary interactions. It is also written so that its content is easy to update.
             This project showed me many new paths to explore and dive deeper into, and there is much more to implement and expand upon going forward.
             I will continue to use this site as playground for new skills I develop.`,
         github: 'https://github.com/Daltraxx/dalton-pettus-portfolio-site',
