4cada00e4b6de5111feaef3d44da1200d7f4fb7f
Dalton Pettus <89285609+Daltraxx@users.noreply.github.com>
2024-07-15 19:43:56 -0700

    Write out project descriptions

--- src/components/Body/Projects/resources/projects-data.js
+++ src/components/Body/Projects/resources/projects-data.js
@@ -3,28 +3,43 @@
 const projectsData = [
     {
         name: 'Ravenous',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam suscipit tortor eros, nec ullamcorper arcu aliquet a. Integer fringilla porttitor metus sagittis aliquam. Nulla sed.',
+        description: 
+            `In this project I built a React app which uses the Yelp API to search and display results based on the business name and location entered. 
+            It is also able to take in different sort parameters with the options of "Best Match", "Highest Rated, or "Most Reviewed.
+            Each search result displays organized information about the business.
+            When one is clicked, a tab is opened with the business's Google Map location.
+            During the course of the project I learned about and paid close attention to the pleasant and functional displaying of information, 
+            adapting to predicted user actions like automatically requerying when the sort option is changed,
+            and ensuring the site is responsive to multiple screen dimensions.`,
         github: 'https://github.com/Daltraxx/ravenous',
         image: '/assets/images/ravenous-project.png',
         technologies: ['JavaScript', 'React', 'Yelp API', 'CSS', 'HTML']
     },
     {
         name: 'Dalton Pettus Portfolio Site',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam suscipit tortor eros, nec ullamcorper arcu aliquet a. Integer fringilla porttitor metus sagittis aliquam. Nulla sed.',
+        description: 
+            `It is worth placing this very site here because of the variety of skills involved in making it. Built with React and making heavy use of props and hooks,
+            the app features multiple pages and dynamic interactions. It is built to be responsive and accessible. I especially enjoyed exploring skeuomorphic touches for many of the buttons,
+            and managing state to dynamically render some of the pages. It is also written to be easy to update.
+            This project showed me many new paths to explore and dive deeper into, and there is much more to implement and expand upon going forward.
+            I will continue to use this site as playground for new skills I develop.`,
         github: 'https://github.com/Daltraxx/dalton-pettus-portfolio-site',
         image: '/assets/images/dalton-portfolio-site-project.png',
         technologies: ['JavaScript', 'React', 'CSS', 'HTML']
     },
     {
         name: 'Life RPG Company Webpage',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam suscipit tortor eros, nec ullamcorper arcu aliquet a. Integer fringilla porttitor metus sagittis aliquam. Nulla sed.',
+        description: 
+            `A simple fake company webpage for a project I am currently studying to eventually build into a production-ready application, 
+            it is included to showcase my ability to work with CSS and HTML to build elegant and responsive webpages from scratch.`,
         github: 'https://github.com/Daltraxx/life-rpg-project-site',
         image: '/assets/images/life-rpg-project.png',
         technologies: ['CSS', 'HTML']
     },
     {
         name: 'Santee Pickleball Webpage',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam suscipit tortor eros, nec ullamcorper arcu aliquet a. Integer fringilla porttitor metus sagittis aliquam. Nulla sed.',
+        description: 
+            `A simple fake pickleball club webpage, it is included to showcase my ability to work with CSS and HTML to build elegant and responsive webpages from scratch.`,
         github: 'https://github.com/Daltraxx/santee-pickleball-site',
         image: '/assets/images/santee-pickleballers-project.png',
         technologies: ['CSS', 'HTML']
