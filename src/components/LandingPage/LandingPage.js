817b13d7d792b79e9b2be223004c5f47e5e2c9cd
Dalton Pettus <89285609+Daltraxx@users.noreply.github.com>
2024-07-21 08:41:13 -0700

    Wrap h1 on landing page in anchor tags

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -6,7 +6,7 @@ import LandingPageNav from './LandingPageNav/LandingPageNav';
 function LandingPage({handlePageChange}) {
     return (
         <>
-            <h1 className={styles.heading}>Dalton Pettus</h1>
+            <h1 className={styles.heading}><a href="# ">Dalton Pettus</a></h1>
             <p className={styles.shortDescription}>
                 An impassioned and prodigiously ambitious developer seeking his first official role in front-end development. 
                 I'm constantly learning new technologies and honing skills both practiced and novel, and am confident that in time I will be a contributing force 
