b1029eab9a57a108fbf6885b6edbdcac43e14057
Dalton Pettus <89285609+Daltraxx@users.noreply.github.com>
2024-07-23 17:13:49 -0700

    Update text for resume work experience

--- src/components/Body/Resume/resources/work-experience.js
+++ src/components/Body/Resume/resources/work-experience.js
@@ -7,14 +7,14 @@ const workExperience = [
         dates: 'Jul 2021 - Jan 2024',
         description: 
             `Working in QA at Robot Cache was my most challenging and satisfying employment to date. 
-            I was promoted to QA Lead despite my relative lack of experience, and learning how to efficiently manage the time and resources of my department was a novel undertaking.
+            I was promoted to QA Lead despite my relative lack of experience, and learning how to effectively manage the time and resources of my department was a novel undertaking.
             My time was largely spent interpreting and responding to the demands of the company and our evolving products, and working with my team to meet those demands.`,
         notes: [
             "Driving force in establishing and sculpting Standard Operating Procedures", 
             "Built workflow and data organization bases using Airtable and Trello from nearly the ground up", 
             "Was challenged and grew greatly in a leadership capacity", 
             "Built automated tests using TestProject.io",
-            "Wrote first for-work scripts to automate required tasks for testing and personell assignment"
+            "Wrote first for-work scripts to automate required tasks for testing and personnel assignment"
         ]
     },
     {
@@ -23,9 +23,9 @@ const workExperience = [
         dates: 'Dec 2018 - Feb 2020',
         description: 
             `This was my first occupation in a tech company, and a major period of personal development as a result.
-            I joined knowing with relatively meager computer experience, but quickly grew comfortable working with software in a professional capacity.
+            I joined with relatively meager IT experience, but quickly grew comfortable working with software in a professional capacity.
             My primary responsibilities involved the software setup and QA of products before they were shipped to clientele, 
-            as well as the office’s technical setup and troubleshooting of any issues that arose.`,
+            as well as the office's technical setup and the troubleshooting of any issues that arose.`,
         notes: [
             "Acquired essential troubleshooting skills",
             "Became comfortable using the command line and executing scripts",
@@ -40,11 +40,11 @@ const workExperience = [
         description: 
             `As the attorney's sole paralegal, I was immediately saddled with vast responsibilities regarding nearly all that concerned the day-to-day operations of the office.
             These included frequent communication with clients and related departments of the government, the writing and preparation of dense and nuanced visa applications,
-            and the building and management of the offices website and social media presence.`,
+            and the building and management of the office's website and social media presence.`,
         notes: [
             "Was granted a great deal of independence to manage an expansive suite of responsibilities",
             "Built and frequently updated the office website using SquareSpace",
-            "Wrote blogs for the office to publish on the office website and highlight on social media",
+            "Wrote blogs for publishing on the office's website and promoted the office on social media",
             "Worked with SEO and Google Analytics to maximize online presence"
         ]
     }
