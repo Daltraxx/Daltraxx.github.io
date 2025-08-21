e883a21ce926e2b5850a2a94f152bebc14282deb
Dalton Pettus <daltpettus@gmail.com>
2025-08-21 10:07:38 -0700

    add nextjs, mongodb, and postgresql to skills

--- src/resources/skills.js
+++ src/resources/skills.js
@@ -1,106 +1,144 @@
 //skills array containing skill information for resume
 
 const skills = [
-    {
-        name: 'JavaScript',
-        alt: 'JavaScript icon',
-        description: "JavaScript is currently my most comfortable programming language by a large margin. I certainly don't know everything, but am confident in my ability to figure out any problem when JavaScript is the tool I'm utlilizing.",
-        src: "/assets/images/js-icon.png",
-        href: '#javascript-skill',
-        id: 'javascript-skill',
-        proficiency: 'skilled'
-    },
-    {
-        name: 'React',
-        alt: 'React icon',
-        description: "Having now built multiple projects using React, I feel secure in my ability to work with the framework in a professional capacity. I know there is much more to learn, and I am excited to begin learning related frameworks such as Next.js and React Native.",
-        src: "/assets/images/react-icon.png", 
-        href: '#react-skill',
-        id: 'react-skill',
-        proficiency: 'skilled'
-    },
-    // {
-    //     name: 'CSS',
-    //     alt: 'CSS icon',
-    //     description: "Once fraught with some difficulties, CSS has become a welcome and familiar tool, and one which I enjoy exploring the possibilities of. One aspect I'm keen on diving deeper into is more complex animations and transitions.",
-    //     src: "/assets/images/css-icon.png", 
-    //     href: '#css-skill',
-    //     id: 'css-skill',
-    //     proficiency: 'skilled'
-    // },
-    // {
-    //     name: 'HTML',
-    //     alt: 'HTML icon',
-    //     description: "I'm as comfortable with HTML as I am anything, and I am also prepared to build pages which adhere to accessibility guidelines and account for SEO best practices.",
-    //     src: "/assets/images/html-icon.png", 
-    //     href: '#html-skill',
-    //     id: 'html-skill',
-    //     proficiency: 'skilled'
-    // },
-    {
-        name: 'Java',
-        alt: 'Java icon',
-        description: "",
-        src: "/assets/images/java-icon.png", 
-        href: '#java-skill',
-        id: 'java-skill',
-        proficiency: 'skilled'
-    },
-    {
-        name: 'TypeScript',
-        alt: 'TypeScript icon',
-        description: "I enjoy using TypeScript and immediately appreciated its ease and type-safe nature (my first type-safe language). I have not encountered any difficulties with it thus far, and will be using it in upcoming personal projects.",
-        src: "/assets/images/typescript-icon.png", 
-        href: '#typescript-skill',
-        id: 'typescript-skill',
-        proficiency: 'experienced'
-    },
-    {
-        name: 'Python',
-        alt: 'Python icon',
-        description: 'My first programming language, it was once my most mastered and experienced, though it has been usurped by JavaScript. Given some minimal refreshing, I would have no discomfort returning to and making use of my Python foundations.',
-        src: "/assets/images/python-icon.png", 
-        href: '#python-skill',
-        id: 'python-skill',
-        proficiency: 'experienced'
-    },
-    {
-        name: 'Handlebars.js',
-        alt: 'Handlebars icon',
-        description: 'I learned HandleBars before I learned React, and - while I could certainly brush off the rust quickly - have not used the framework in great frequency since becoming familiar with React.',
-        src: "/assets/images/handlebars-icon.png", 
-        href: '#handlebarsjs-skill',
-        id: 'handlebarsjs-skill',
-        proficiency: 'experienced'
-    },
-    {
-        name: 'Node.js',
-        alt: 'Node.js icon',
-        description: "Node.js is relatively recent to me, and while I've completed the Codecademy course, I have much to learn and absorb more fully regarding it's possibilities. Mastering this language is one of my ongoing endeavors.",
-        src: "/assets/images/node-js-icon.png", 
-        href: '#nodejs-skill',
-        id: 'nodejs-skill',
-        proficiency: 'experienced'
-    },
-    {
-        name: 'Bootstrap',
-        alt: 'Bootstrap icon',
-        description: "Feeling the need to know a CSS/HTML framework, I learned Bootstrap and grew to appreciate its ease of use. I still enjoy building sites from CSS scratch, but am able to use Bootstrap for components I haven't yet learned to implement myself.",
-        src: "/assets/images/bootstrap-icon.png", 
-        href: '#bootstrap-skill',
-        id: 'bootstrap-skill',
-        proficiency: 'experienced'
-    },
-    {
-        name: 'Angular.js',
-        alt: 'Angular.js icon',
-        description: "Like Handlebars.js, I learned the basics of Angular before moving on to React. That said, I'm confident in my ability to tackle projects in Angular if needed.",
-        src: "/assets/images/angular-icon.png", 
-        href: '#angularjs-skill',
-        id: 'angularjs-skill',
-        proficiency: 'experienced'
-    },
-    /*
+  {
+    name: "JavaScript",
+    alt: "JavaScript icon",
+    description:
+      "JavaScript is currently my most comfortable programming language by a large margin. I certainly don't know everything, but am confident in my ability to figure out any problem when JavaScript is the tool I'm utlilizing.",
+    src: "/assets/images/js-icon.png",
+    href: "#javascript-skill",
+    id: "javascript-skill",
+    proficiency: "skilled",
+  },
+  {
+    name: "React",
+    alt: "React icon",
+    description:
+      "Having now built multiple projects using React, I feel secure in my ability to work with the framework in a professional capacity. I know there is much more to learn, and I am excited to begin learning related frameworks such as Next.js and React Native.",
+    src: "/assets/images/react-icon.png",
+    href: "#react-skill",
+    id: "react-skill",
+    proficiency: "skilled",
+  },
+  {
+    name: "Next.js",
+    alt: "Next.js icon",
+    description:
+      "Next.js is a React framework that enables server-side rendering and static site generation. I have started exploring its features and appreciate its ability to improve performance and SEO for web applications.",
+    src: "/assets/images/nextjs-icon.webp",
+    href: "#nextjs-skill",
+    id: "nextjs-skill",
+    proficiency: "experienced",
+  },
+  {
+    name: "TypeScript",
+    alt: "TypeScript icon",
+    description:
+      "I enjoy using TypeScript and immediately appreciated its ease and type-safe nature (my first type-safe language). I have not encountered any difficulties with it thus far, and will be using it in upcoming personal projects.",
+    src: "/assets/images/typescript-icon.png",
+    href: "#typescript-skill",
+    id: "typescript-skill",
+    proficiency: "experienced",
+  },
+  {
+    name: "MongoDB",
+    alt: "MongoDB icon",
+    description:
+      "MongoDB is a NoSQL database that I have used for storing and managing data in web applications. I am familiar with its document-oriented model and basic operations.",
+    src: "/assets/images/mongodb-icon.webp",
+    href: "#mongodb-skill",
+    id: "mongodb-skill",
+    proficiency: "skilled",
+  },
+  {
+    name: "PostgreSQL",
+    alt: "PostgreSQL icon",
+    description:
+      "PostgreSQL is a powerful, open-source relational database system. I have used it for managing structured data and am familiar with its SQL querying capabilities.",
+    src: "/assets/images/postgresql-icon.webp",
+    href: "#postgresql-skill",
+    id: "postgresql-skill",
+    proficiency: "skilled",
+  },
+  // {
+  //     name: 'CSS',
+  //     alt: 'CSS icon',
+  //     description: "Once fraught with some difficulties, CSS has become a welcome and familiar tool, and one which I enjoy exploring the possibilities of. One aspect I'm keen on diving deeper into is more complex animations and transitions.",
+  //     src: "/assets/images/css-icon.png",
+  //     href: '#css-skill',
+  //     id: 'css-skill',
+  //     proficiency: 'skilled'
+  // },
+  // {
+  //     name: 'HTML',
+  //     alt: 'HTML icon',
+  //     description: "I'm as comfortable with HTML as I am anything, and I am also prepared to build pages which adhere to accessibility guidelines and account for SEO best practices.",
+  //     src: "/assets/images/html-icon.png",
+  //     href: '#html-skill',
+  //     id: 'html-skill',
+  //     proficiency: 'skilled'
+  // },
+  {
+    name: "Node.js",
+    alt: "Node.js icon",
+    description:
+      "Node.js is relatively recent to me, and while I've completed the Codecademy course, I have much to learn and absorb more fully regarding it's possibilities. Mastering this language is one of my ongoing endeavors.",
+    src: "/assets/images/node-js-icon.png",
+    href: "#nodejs-skill",
+    id: "nodejs-skill",
+    proficiency: "experienced",
+  },
+  {
+    name: "Java",
+    alt: "Java icon",
+    description: "",
+    src: "/assets/images/java-icon.png",
+    href: "#java-skill",
+    id: "java-skill",
+    proficiency: "skilled",
+  },
+  {
+    name: "Python",
+    alt: "Python icon",
+    description:
+      "My first programming language, it was once my most mastered and experienced, though it has been usurped by JavaScript. Given some minimal refreshing, I would have no discomfort returning to and making use of my Python foundations.",
+    src: "/assets/images/python-icon.png",
+    href: "#python-skill",
+    id: "python-skill",
+    proficiency: "experienced",
+  },
+  {
+    name: "Handlebars.js",
+    alt: "Handlebars icon",
+    description:
+      "I learned HandleBars before I learned React, and - while I could certainly brush off the rust quickly - have not used the framework in great frequency since becoming familiar with React.",
+    src: "/assets/images/handlebars-icon.png",
+    href: "#handlebarsjs-skill",
+    id: "handlebarsjs-skill",
+    proficiency: "experienced",
+  },
+  {
+    name: "Bootstrap",
+    alt: "Bootstrap icon",
+    description:
+      "Feeling the need to know a CSS/HTML framework, I learned Bootstrap and grew to appreciate its ease of use. I still enjoy building sites from CSS scratch, but am able to use Bootstrap for components I haven't yet learned to implement myself.",
+    src: "/assets/images/bootstrap-icon.png",
+    href: "#bootstrap-skill",
+    id: "bootstrap-skill",
+    proficiency: "experienced",
+  },
+  {
+    name: "Angular.js",
+    alt: "Angular.js icon",
+    description:
+      "Like Handlebars.js, I learned the basics of Angular before moving on to React. That said, I'm confident in my ability to tackle projects in Angular if needed.",
+    src: "/assets/images/angular-icon.png",
+    href: "#angularjs-skill",
+    id: "angularjs-skill",
+    proficiency: "experienced",
+  },
+  /*
     {
         name: 'C++',
         alt: 'C++ icon',
@@ -111,6 +149,6 @@ const skills = [
         proficiency: 'experienced'
     },
     */
-]
+];
 
 export default skills;
