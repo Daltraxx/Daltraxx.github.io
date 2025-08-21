d39b19f4dadd724480d0c52b8edbf89b0ad5425a
Dalton Pettus <daltpettus@gmail.com>
2025-08-20 18:12:07 -0700

    reduce SkillCarousel to one skill icon display

--- src/components/LandingPage/SkillCarousel/SkillCarousel.js
+++ src/components/LandingPage/SkillCarousel/SkillCarousel.js
@@ -5,38 +5,28 @@ import skills from '../../../resources/skills';
 
 function SkillCarousel({ handlePageChange }) {
     //create arrays for displaying "proficient in" and "experience with" icons
-    const skillIconsDisplay = [];
-    const experienceIconsDisplay = [];
+    const skillIconDisplay = [];
     for (let skill of skills) {
-        if (skill.proficiency === 'skilled') {
-            skillIconsDisplay.push
-            (
-                <div className={styles.iconContainer} key={skill.name}>
-                    <a href='#skills' onClick={handlePageChange}><img src={skill.src} alt={skill.alt} title={skill.name} name='resume' /></a>
-                </div>
-            );
-        } else {
-            experienceIconsDisplay.push
-            (
-                <div className={styles.iconContainer} key={skill.name}>
-                    <a href='#skills' onClick={handlePageChange}><img src={skill.src} alt={skill.alt} title={skill.name} name='resume' /></a>
-                </div>
-            );
-        }  
+        skillIconDisplay.push(
+            <div className={styles.iconContainer} key={skill.name}>
+                <a href="#skills" onClick={handlePageChange}>
+                    <img
+                        src={skill.src}
+                        alt={skill.alt}
+                        title={skill.name}
+                        name="resume"
+                    />
+                </a>
+            </div>
+        );
     };
     
     return (
         <section className={styles.skillsSection}>
             <div className={styles.container}>
-                <h2>Proficient in:</h2>
+                <h2>Experience With</h2>
                 <div className={styles.iconDisplay}>
-                    {skillIconsDisplay}
-                </div>
-            </div>
-            <div className={styles.container}>
-                <h2>Experience with:</h2>
-                <div className={styles.iconDisplay}>
-                    {experienceIconsDisplay}
+                    {skillIconDisplay}
                 </div>
             </div>
         </section>
