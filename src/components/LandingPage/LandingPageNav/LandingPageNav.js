e1cfbe6a1f105d857329587b5ab4e1a08e90cef4
Dalton Pettus <daltpettus@gmail.com>
2025-08-21 11:16:27 -0700

    prettier formatting

--- src/components/LandingPage/LandingPageNav/LandingPageNav.js
+++ src/components/LandingPage/LandingPageNav/LandingPageNav.js
@@ -4,13 +4,49 @@ function LandingPageNav({handlePageChange}) {
     return (
         <nav className={styles.nav}>
             <ul>
-                <li key="about"><a href="#!" className={styles.leftNavButton} onClick={handlePageChange} name="about">About</a></li>
-                <li key="resume"><a href="#!" className={styles.leftNavButton} onClick={handlePageChange} name="resume">Resume</a></li>
-                <li key="projects"><a href="#!" className={styles.rightNavButton} onClick={handlePageChange} name="projects">Projects</a></li>
-                <li key="contact"><a href="#!" className={styles.rightNavButton} onClick={handlePageChange} name="contact">Contact</a></li>
+                <li key="about">
+                    <a
+                        href="#!"
+                        className={styles.leftNavButton}
+                        onClick={handlePageChange}
+                        name="about"
+                    >
+                        About
+                    </a>
+                </li>
+                <li key="resume">
+                    <a
+                        href="#!"
+                        className={styles.leftNavButton}
+                        onClick={handlePageChange}
+                        name="resume"
+                    >
+                        Resume
+                    </a>
+                </li>
+                <li key="projects">
+                    <a
+                        href="#!"
+                        className={styles.rightNavButton}
+                        onClick={handlePageChange}
+                        name="projects"
+                    >
+                        Projects
+                    </a>
+                </li>
+                <li key="contact">
+                    <a
+                        href="#!"
+                        className={styles.rightNavButton}
+                        onClick={handlePageChange}
+                        name="contact"
+                    >
+                        Contact
+                    </a>
+                </li>
             </ul>
         </nav>
-    )
+    );
 }
 
 export default LandingPageNav;
