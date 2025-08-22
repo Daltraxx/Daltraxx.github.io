c55b2d44b33bb19add73297988baff67e850c8d3
Dalton Pettus <daltpettus@gmail.com>
2025-08-22 11:31:15 -0700

    add aria label to name link

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -6,7 +6,7 @@ import LandingPageNav from './LandingPageNav/LandingPageNav';
 function LandingPage({handlePageChange}) {
     return (
         <>
-            <h1 className={styles.heading}><a href="# ">Dalton Pettus</a></h1>
+            <h1 className={styles.heading}><a href="#" aria-label="Home">Dalton Pettus</a></h1>
             <p className={styles.shortDescription}>
                 Endlessly curious, ambitious software engineer passionate about learning and building. 
                 Instrumental in servicing any person or organization looking to meet their project or development goals.
