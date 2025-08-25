066387a71a7d40d57d75b655b8ca2fabfc1f7b37
Dalton Pettus <daltpettus@gmail.com>
2025-08-25 10:29:12 -0700

    wording change

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -5,16 +5,22 @@ import LandingPageNav from './LandingPageNav/LandingPageNav';
 
 function LandingPage({handlePageChange}) {
     return (
-        <>
-            <h1 className={styles.heading}><a href="/" aria-current="page">Dalton Pettus</a></h1>
-            <p className={styles.shortDescription}>
-                Endlessly curious, ambitious software engineer passionate about learning and building. 
-                Instrumental in servicing any person or organization looking to meet their project or development goals.
-                Work with someone who loves what they do &mdash; reach out and let's have a conversation.
-            </p>
-            <LandingPageNav handlePageChange={handlePageChange}/>
-        </>
-    )
+      <>
+        <h1 className={styles.heading}>
+          <a href="/" aria-current="page">
+            Dalton Pettus
+          </a>
+        </h1>
+        <p className={styles.shortDescription}>
+          An endlessly curious, ambitious software engineer passionate about
+          learning and building. Instrumental in servicing any person or
+          organization looking to meet their project or development goals. Work
+          with someone who loves what they do — reach out and let's have a
+          conversation.
+        </p>
+        <LandingPageNav handlePageChange={handlePageChange} />
+      </>
+    );
 }
 
 export default LandingPage;
