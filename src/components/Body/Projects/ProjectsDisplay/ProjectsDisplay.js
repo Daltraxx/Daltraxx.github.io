0bb1f9fa1378a5597d623409c475f43ce0535984
Dalton Pettus <daltpettus@gmail.com>
2025-08-25 14:30:39 -0700

    add aria label and referrer policy no referrer to project links

--- src/components/Body/Projects/ProjectsDisplay/ProjectsDisplay.js
+++ src/components/Body/Projects/ProjectsDisplay/ProjectsDisplay.js
@@ -5,27 +5,41 @@ function ProjectsDisplay({ projectsData }) {
     //get jsx for each project by mapping from projectsData array of objects
     const projectsJSX = projectsData.map((project, index) => {
         return (
-            <section className={styles.projectContainer} key={`project${index}`}>
-                <section className={styles.projectInfoHalf}>
-                    <div className={styles.imageContainer} style={{ backgroundImage: `url(${project.image})` }} aria-hidden="true">
-                        <div className={styles.shader}>
-                            <div className={styles.titleInfo}>
-                                <h3>{project.name}</h3>
-                                <h4>Technologies Used:</h4>
-                                <ul>
-                                    {project.technologies.map(tech => <li key={`${tech}`}>{tech}</li>)}
-                                </ul>
-                                <a href={project.github} target="_blank" rel='noopener noreferrer'>View on GitHub</a>
-                            </div>
-                        </div>
-                    </div>
-                </section>
-                <section className={styles.projectInfoHalf}>
-                    <div className={styles.projectDetails}>
-                        <p>{project.description}</p>
-                    </div>
-                </section>
+          <section className={styles.projectContainer} key={`project${index}`}>
+            <section className={styles.projectInfoHalf}>
+              <div
+                className={styles.imageContainer}
+                style={{ backgroundImage: `url(${project.image})` }}
+                aria-hidden="true"
+              >
+                <div className={styles.shader}>
+                  <div className={styles.titleInfo}>
+                    <h3>{project.name}</h3>
+                    <h4>Technologies Used:</h4>
+                    <ul>
+                      {project.technologies.map((tech) => (
+                        <li key={`${tech}`}>{tech}</li>
+                      ))}
+                    </ul>
+                    <a
+                      href={project.github}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      aria-label={`View ${project.name} on GitHub`}
+                      referrerPolicy="no-referrer"
+                    >
+                      View on GitHub
+                    </a>
+                  </div>
+                </div>
+              </div>
             </section>
+            <section className={styles.projectInfoHalf}>
+              <div className={styles.projectDetails}>
+                <p>{project.description}</p>
+              </div>
+            </section>
+          </section>
         );
     });
 
