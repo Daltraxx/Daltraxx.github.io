0a5effa20504fd602cde11bebced613dfb2b9e43
Dalton Pettus <daltpettus@gmail.com>
2025-08-26 10:43:03 -0700

    rename to socialLinksContainer

--- src/components/Header/SocialLinks/SocialLinks.js
+++ src/components/Header/SocialLinks/SocialLinks.js
@@ -3,7 +3,7 @@ import styles from './SocialLinks.module.css';
 
 function SocialLinks({ className = ''}) {
     return (
-        <ul className={clsx(styles.socialLinksUl, className)}>
+        <ul className={clsx(styles.socialLinksContainer, className)}>
             <li><a href="https://github.com/Daltraxx" target="_blank"  rel="noreferrer">GitHub</a></li>
             <li><a href="https://www.linkedin.com/in/dalton-pettus/" target="_blank"  rel="noreferrer">LinkedIn</a></li>
         </ul>
