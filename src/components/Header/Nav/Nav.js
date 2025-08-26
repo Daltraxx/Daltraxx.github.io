6a604c7545baf0d1a7b7b8f81907638b1cbb62ea
Dalton Pettus <daltpettus@gmail.com>
2025-08-26 14:04:07 -0700

    format

--- src/components/Header/Nav/Nav.js
+++ src/components/Header/Nav/Nav.js
@@ -16,13 +16,49 @@ function Nav({ handlePageChange, currentPage }) {
     return (
         <nav className={styles.navContainer}>
             <ul>
-                <li><a href="#!" className={getClassName("about")} onClick={handlePageChange} name="about">About</a></li>
-                <li><a href="#!" className={getClassName("resume")} onClick={handlePageChange} name="resume">Resume</a></li>
-                <li><a href="#!" className={getClassName("projects")} onClick={handlePageChange} name="projects">Projects</a></li>
-                <li><a href="#!" className={getClassName("contact")} onClick={handlePageChange} name="contact">Contact</a></li>
+                <li>
+                    <a
+                        href="#!"
+                        className={getClassName("about")}
+                        onClick={handlePageChange}
+                        name="about"
+                    >
+                        About
+                    </a>
+                </li>
+                <li>
+                    <a
+                        href="#!"
+                        className={getClassName("resume")}
+                        onClick={handlePageChange}
+                        name="resume"
+                    >
+                        Resume
+                    </a>
+                </li>
+                <li>
+                    <a
+                        href="#!"
+                        className={getClassName("projects")}
+                        onClick={handlePageChange}
+                        name="projects"
+                    >
+                        Projects
+                    </a>
+                </li>
+                <li>
+                    <a
+                        href="#!"
+                        className={getClassName("contact")}
+                        onClick={handlePageChange}
+                        name="contact"
+                    >
+                        Contact
+                    </a>
+                </li>
             </ul>
         </nav>
-    )
+    );
 }
 
 export default Nav;
