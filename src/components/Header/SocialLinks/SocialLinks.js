1ef79d41aaaa2cbbc969ba0e4cc56ca97d17ee58
Dalton Pettus <daltpettus@gmail.com>
2025-08-26 13:16:35 -0700

    add rel noopener noreferrer to social links

--- src/components/Header/SocialLinks/SocialLinks.js
+++ src/components/Header/SocialLinks/SocialLinks.js
@@ -4,9 +4,8 @@ import styles from './SocialLinks.module.css';
 function SocialLinks({ className = ''}) {
     return (
         <ul className={clsx(styles.socialLinksContainer, className)}>
-            <li><a href="https://github.com/Daltraxx" target="_blank"  rel="noreferrer">GitHub</a></li>
-            <li><a href="https://www.linkedin.com/in/dalton-pettus/" target="_blank"  rel="noreferrer">LinkedIn</a></li>
-        </ul>
+            <li><a href="https://github.com/Daltraxx" target="_blank"  rel="noopener noreferrer">GitHub</a></li>
+            <li><a href="https://www.linkedin.com/in/dalton-pettus/" target="_blank"  rel="noopener noreferrer">LinkedIn</a></li>        </ul>
     )
 }
 
