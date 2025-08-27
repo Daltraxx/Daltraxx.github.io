a1a04b8ee5bd240e02f76cb2ed60a4a1648294a4
Dalton Pettus <daltpettus@gmail.com>
2025-08-26 18:08:21 -0700

    add aria-current to nav buttons

--- src/components/Header/Nav/Nav.js
+++ src/components/Header/Nav/Nav.js
@@ -14,50 +14,54 @@ function Nav({ handlePageChange, currentPage }) {
 
 
     return (
-        <nav className={styles.navContainer}>
-            <ul>
-                <li>
-                    <a
-                        href="#!"
-                        className={getClassName("about")}
-                        onClick={handlePageChange}
-                        data-page="about"
-                    >
-                        About
-                    </a>
-                </li>
-                <li>
-                    <a
-                        href="#!"
-                        className={getClassName("resume")}
-                        onClick={handlePageChange}
-                        data-page="resume"
-                    >
-                        Resume
-                    </a>
-                </li>
-                <li>
-                    <a
-                        href="#!"
-                        className={getClassName("projects")}
-                        onClick={handlePageChange}
-                        data-page="projects"
-                    >
-                        Projects
-                    </a>
-                </li>
-                <li>
-                    <a
-                        href="#!"
-                        className={getClassName("contact")}
-                        onClick={handlePageChange}
-                        data-page="contact"
-                    >
-                        Contact
-                    </a>
-                </li>
-            </ul>
-        </nav>
+      <nav className={styles.navContainer}>
+        <ul>
+          <li>
+            <a
+              href="#!"
+              className={getClassName("about")}
+              onClick={handlePageChange}
+              data-page="about"
+              aria-current={currentPage === "about" ? "page" : undefined}
+            >
+              About
+            </a>
+          </li>
+          <li>
+            <a
+              href="#!"
+              className={getClassName("resume")}
+              onClick={handlePageChange}
+              data-page="resume"
+              aria-current={currentPage === "resume" ? "page" : undefined}
+            >
+              Resume
+            </a>
+          </li>
+          <li>
+            <a
+              href="#!"
+              className={getClassName("projects")}
+              onClick={handlePageChange}
+              data-page="projects"
+              aria-current={currentPage === "projects" ? "page" : undefined}
+            >
+              Projects
+            </a>
+          </li>
+          <li>
+            <a
+              href="#!"
+              className={getClassName("contact")}
+              onClick={handlePageChange}
+              data-page="contact"
+              aria-current={currentPage === "contact" ? "page" : undefined}
+            >
+              Contact
+            </a>
+          </li>
+        </ul>
+      </nav>
     );
 }
 
