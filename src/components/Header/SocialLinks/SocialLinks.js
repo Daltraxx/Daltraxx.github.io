0169bfb09447f123d13086b01607f6002bcb3790
Dalton Pettus <daltpettus@gmail.com>
2025-08-26 18:45:25 -0700

    give aria label Social Links to SocialLinks ul

--- src/components/Header/SocialLinks/SocialLinks.js
+++ src/components/Header/SocialLinks/SocialLinks.js
@@ -3,10 +3,30 @@ import styles from './SocialLinks.module.css';
 
 function SocialLinks({ className }) {
     return (
-        <ul className={clsx(styles.socialLinksContainer, className)}>
-            <li><a href="https://github.com/Daltraxx" target="_blank"  rel="noopener noreferrer">GitHub</a></li>
-            <li><a href="https://www.linkedin.com/in/dalton-pettus/" target="_blank"  rel="noopener noreferrer">LinkedIn</a></li>        </ul>
-    )
+      <ul
+        className={clsx(styles.socialLinksContainer, className)}
+        aria-label="Social links"
+      >
+        <li>
+          <a
+            href="https://github.com/Daltraxx"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GitHub
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.linkedin.com/in/dalton-pettus/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            LinkedIn
+          </a>
+        </li>{" "}
+      </ul>
+    );
 }
 
 export default SocialLinks;
