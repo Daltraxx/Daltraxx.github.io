641bb0a14dacdf93e094f32f0d9feefd889be85e
Dalton Pettus <daltpettus@gmail.com>
2026-01-26 19:48:55 -0800

    grammar and formatting

--- src/components/Body/Resume/resources/work-experience.js
+++ src/components/Body/Resume/resources/work-experience.js
@@ -26,9 +26,12 @@ const workExperience = [
     title: "Software Developer and IT Consultant",
     company: "Contract Work",
     dates: "Jul 2025 - Present",
-    description: `As a software developer and IT consultant, I have found and implemented full-stack solutions tailored to clients qualitative and economic needs. 
-            It has by far been the most rewarding work of my career, while also offering flexibility for me to explore new technologies and frameworks.
-            I always deliver high-quality, extendable, and accessible products, for which clients have rewarded me by continuing to request my services as future needs arise.`,
+    description: `As a software developer and IT consultant, I have found and implemented 
+            full-stack solutions tailored to clients' qualitative and economic needs. 
+            It has by far been the most rewarding work of my career, while also offering flexibility
+            for me to explore new technologies and frameworks.
+            I always deliver high-quality, extendable, and accessible products, for which clients
+            have rewarded me by continuing to request my services as future needs arise.`,
     notes: [
       "Developed and deployed full-stack web applications using Next.js and PostgreSQL",
       "Integrated products with CMS platforms such as Prismic to allow clients to easily manage content",
@@ -41,8 +44,10 @@ const workExperience = [
     company: "Robot Cache",
     dates: "Jul 2021 - Jan 2024",
     description: `Working in QA at Robot Cache was my most challenging and rewarding employment to date. 
-            I was promoted to QA Lead despite my relative lack of experience, and learning how to effectively manage the time and resources of my department was a novel undertaking.
-            My time was largely spent interpreting and responding to the demands of the company and our evolving products, and working with my team to meet those demands.`,
+            I was promoted to QA Lead despite my relative lack of experience, and learning how to effectively
+            manage the time and resources of my department was a novel undertaking.
+            My time was largely spent interpreting and responding to the demands of the company and our
+            evolving products, and working with my team to meet those demands.`,
     notes: [
       "Driving force in establishing and sculpting Standard Operating Procedures",
       "Built workflow and data organization bases using Airtable and Trello from nearly the ground up",
@@ -55,10 +60,11 @@ const workExperience = [
     title: "QA Analyst and IT",
     company: "Aira Tech Corp",
     dates: "Dec 2018 - Feb 2020",
-    description: `This was my first occupation in a tech company, and a major period of personal development as a result.
-            I joined with relatively meager IT experience, but quickly grew comfortable working with software in a professional capacity.
-            My primary responsibilities involved the software setup and QA of products before they were shipped to clientele, 
-            as well as the office's technical setup and the troubleshooting of any issues that arose.`,
+    description: `This was my first occupation in a tech company, and a major period of personal development as a 
+            result. I joined with relatively meager IT experience, but quickly grew comfortable working with software
+            in a professional capacity.My primary responsibilities involved the software setup and QA of products
+            before they were shipped to clientele, as well as the office's technical setup and the
+            troubleshooting of any issues that arose.`,
     notes: [
       "Acquired essential troubleshooting skills",
       "Became comfortable using the command line and executing scripts",
