e7ef6bc1e2923351ac5bb26b65fa386cbf430d26
Dalton Pettus <daltpettus@gmail.com>
2026-04-08 18:45:50 -0700

    replace links in SkillCarousel

--- src/components/LandingPage/SkillCarousel/SkillCarousel.js
+++ src/components/LandingPage/SkillCarousel/SkillCarousel.js
@@ -1,43 +1,34 @@
-import styles from './SkillCarousel.module.css';
+import styles from "./SkillCarousel.module.css";
 
-import skills from '../../../resources/skills';
-import clsx from 'clsx';
+import skills from "../../../resources/skills";
+import clsx from "clsx";
+import { Link } from "react-router";
 
+function SkillCarousel({ handlePageChange, className = "" }) {
 
-function SkillCarousel({ handlePageChange, className = '' }) {
-    const handleSkillClick = (e) => {
-        e.preventDefault();
-        handlePageChange(e);
-    };
+  const skillIconDisplay = skills.map((skill) => (
+    <div className={styles.iconContainer} key={skill.id}>
+      <Link
+        to="/resume"
+        aria-label="Navigate to Resume page"
+        className={styles.iconLink}
+      >
+        <img
+          src={skill.src}
+          alt={skill.alt || skill.name}
+          width={skill.width ?? 70}
+          height={skill.height ?? 70}
+        />
+      </Link>
+    </div>
+  ));
 
-    const skillIconDisplay = skills.map((skill) => (
-        <div
-            className={styles.iconContainer}
-            key={skill.id}
-        >
-            <button
-                onClick={handleSkillClick}
-                data-page="resume"
-                aria-label={`Navigate to Resume page`}
-            >
-                <img
-                    src={skill.src}
-                    alt={skill.alt || skill.name}
-                    width={skill.width ?? 70}
-                    height={skill.height ?? 70}
-                />
-            </button>
-        </div>
-    ));
-    
-    return (
-        <section className={clsx(styles.skillsSection, className)}>
-            <h2>Experience With</h2>
-            <div className={styles.iconDisplay}>
-                {skillIconDisplay}
-            </div>
-        </section>
-    )
+  return (
+    <section className={clsx(styles.skillsSection, className)}>
+      <h2>Experience With</h2>
+      <div className={styles.iconDisplay}>{skillIconDisplay}</div>
+    </section>
+  );
 }
 
 export default SkillCarousel;
