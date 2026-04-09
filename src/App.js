d0d0ac49186a7b80e8518e6fc2416b4a704b2fcb
Dalton Pettus <daltpettus@gmail.com>
2026-04-08 19:09:34 -0700

    remove unused imports

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
-import { useState, useMemo, useCallback } from 'react';
-import { Helmet } from 'react-helmet-async';
-import styles from './App.module.css';
-import MainLayout from './components/MainLayout/MainLayout';
-import personJsonLd from './lib/jsonLd';
-import { Routes, Route } from 'react-router';
+import { useState, useMemo } from "react";
+import { Helmet } from "react-helmet-async";
+import MainLayout from "./components/MainLayout/MainLayout";
+import personJsonLd from "./lib/jsonLd";
+import { Routes, Route } from "react-router";
 
-// TODO: Delete these once react-router is fully implemented and tested
-import Header from "./components/MainLayout/Header/Header";
-import Footer from "./components/MainLayout/Footer/Footer";
 import LandingPage from "./components/LandingPage/LandingPage";
 import About from "./components/Body/About/About";
 import Resume from "./components/Body/Resume/Resume";
@@ -19,7 +15,7 @@ function App() {
 
   const ldJson = useMemo(
     () => JSON.stringify(personJsonLd).replace(/</g, "\\u003c"),
-    []
+    [],
   );
 
   return (
@@ -29,28 +25,13 @@ function App() {
       </Helmet>
       <Routes>
         {/* Landing Page */}
-        <Route
-          path="/"
-          element={<LandingPage />}
-        />
+        <Route path="/" element={<LandingPage />} />
         {/* Main Layout and nested pages */}
         <Route element={<MainLayout />}>
-          <Route
-            path="/about"
-            element={<About />}
-          />
-          <Route
-            path="/resume"
-            element={<Resume />}
-          />
-          <Route
-            path="/projects"
-            element={<Projects />}
-          />
-          <Route
-            path="/contact"
-            element={<Contact />}
-          />
+          <Route path="/about" element={<About />} />
+          <Route path="/resume" element={<Resume />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
         </Route>
       </Routes>
     </>
