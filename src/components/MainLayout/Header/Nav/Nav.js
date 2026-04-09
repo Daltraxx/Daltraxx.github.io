ba1e84fb44598b32b76807d0fa972b9442e80d4f
Dalton Pettus <daltpettus@gmail.com>
2026-04-08 17:46:18 -0700

    add NavLinks to main nav

--- src/components/MainLayout/Header/Nav/Nav.js
+++ src/components/MainLayout/Header/Nav/Nav.js
@@ -1,39 +1,27 @@
-import clsx from 'clsx';
-import styles from './Nav.module.css';
-import navItems from '../../../../resources/navItems';
-
+import clsx from "clsx";
+import styles from "./Nav.module.css";
+import navItems from "../../../../resources/navItems";
+import { NavLink } from "react-router";
 
 function Nav({ handlePageChange, currentPage }) {
-    const getClassName = (page) =>
-      page === currentPage
-        ? clsx(
-            styles.navButton,
-            styles.current
-          )
-        : styles.navButton;
-    
-
-
-    return (
-      <nav className={styles.navContainer} aria-label='Primary'>
-        <ul>
-            {navItems.map(({ id, label }) => (
-                <li key={id}>
-                    <a
-                        href="#"
-                        className={getClassName(id)}
-                        onClick={handlePageChange}
-                        data-page={id}
-                        role='button'
-                        aria-current={currentPage === id ? "page" : undefined}
-                    >
-                        {label}
-                    </a>
-                </li>
-            ))}
-        </ul>
-      </nav>
-    );
+  return (
+    <nav className={styles.navContainer} aria-label="Primary">
+      <ul>
+        {navItems.map(({ id, label }) => (
+          <li key={id}>
+            <NavLink
+              to={`/${id}`}
+              className={({ isActive }) =>
+                clsx(styles.navButton, isActive && styles.current)
+              }
+            >
+              {label}
+            </NavLink>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
 }
 
 export default Nav;
