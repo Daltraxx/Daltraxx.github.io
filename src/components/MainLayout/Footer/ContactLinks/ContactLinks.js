728a34cf2f6b8a7ea3f63eacc0f203a7109f09f0
Dalton Pettus <daltpettus@gmail.com>
2026-04-08 19:27:33 -0700

    remove unnecessary aria labels

--- src/components/MainLayout/Footer/ContactLinks/ContactLinks.js
+++ src/components/MainLayout/Footer/ContactLinks/ContactLinks.js
@@ -1,39 +1,38 @@
-import styles from './ContactLinks.module.css';
+import styles from "./ContactLinks.module.css";
 
-import handleEmail from '../../../../utils/handleEmail';
+import handleEmail from "../../../../utils/handleEmail";
 
 function ContactLinks() {
+  const linkedInLink = (
+    <a
+      href="https://www.linkedin.com/in/dalton-pettus/"
+      target="_blank"
+      rel="noreferrer"
+      className={styles.contactButton}
+    >
+      LinkedIn
+    </a>
+  );
 
-    const linkedInLink = <a 
-        href="https://www.linkedin.com/in/dalton-pettus/" 
-        target="_blank"
-        rel="noreferrer"
-        className={styles.contactButton} 
-        aria-label="LinkedIn link">
-        LinkedIn
-        </a>;
+  const emailLink = (
+    <a
+      href="#!"
+      className={styles.contactButton}
+      onClick={handleEmail}
+      id="contact-link-footer"
+    >
+      Email
+    </a>
+  );
 
-    const emailLink = <a 
-        href='#!' 
-        className={styles.contactButton} 
-        onClick={handleEmail} 
-        id='contact-link-footer' 
-        aria-label="email button">
-        Email
-        </a>;
-        
-    return (
-        <section className={styles.contactLinksSection}>
-            <ul>
-                <li>
-                    {linkedInLink}
-                </li>
-                <li>
-                    {emailLink}
-                </li>
-            </ul>
-        </section>
-    )
+  return (
+    <section className={styles.contactLinksSection}>
+      <ul>
+        <li>{linkedInLink}</li>
+        <li>{emailLink}</li>
+      </ul>
+    </section>
+  );
 }
 
 export default ContactLinks;
